ec4fb318af5202d0de1ec24c3f35000e137187f0
belaifa el hussein <belaifahocine@gmail.com>
2019-07-09 11:52:08 +0200

    create event type & create event dummy data & fetch event with graphql interface

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const graphQlHttp=require('express-graphql')
 const {buildSchema}=require('graphql')
 
 const app = express();
+const events=[];
 
 app.use(bodyParser.json());
 
@@ -11,14 +12,32 @@ app.use(bodyParser.json());
 
 app.use('/graphql',graphQlHttp({
     schema:buildSchema(`
+    type Event {
+        _id:ID!
+        title:String!
+        description:String! 
+        price:Float! 
+        date:String! 
+
+
+    }
+
+    input EventInput {
+        title:String!
+        description:String! 
+        price:Float! 
+        date:String! 
+
+    }
+
     type RootQuery {
-        events:[String!]!
+        events:[Event!]!
 
     }
 
     type RootMutation{
 
-        createEvent(name:String):String
+        createEvent(eventInput:EventInput):Event
 
     }
      schema {
@@ -29,17 +48,24 @@ app.use('/graphql',graphQlHttp({
     `),
     rootValue:{
         events:()=>{
-            return [
-                'Romantic',
-                'Cooking',
-                'All night coding'
-            ]
+            return events
         },
         createEvent:(args)=>{
 
-            const eventName=args.name
+            const event={
+                _id:Math.random().toString(),
+                title:args.eventInput.title,
+                description:args.eventInput.description,
+                price:+args.eventInput.price,
+                date:args.eventInput.date
 
-            return eventName
+            }
+
+            events.push(event);
+
+           
+          return event;
+           
 
         }
     },
