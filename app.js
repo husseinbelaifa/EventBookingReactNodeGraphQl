b5260be2e15ec5142e1d77a8d3ae5bfd5f3a66d1
belaifa el hussein <belaifahocine@gmail.com>
2019-07-09 15:06:35 +0200

    add realtion with event & user & create resolver for it

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const graphQlHttp=require('express-graphql')
-const {buildSchema}=require('graphql')
-const mongoose=require('mongoose')
-const Event=require('./models/event')
+const graphQlHttp = require("express-graphql");
+const { buildSchema } = require("graphql");
+const mongoose = require("mongoose");
+const Event = require("./models/event");
+const User = require("./models/user");
+const bcrypt = require("bcryptjs");
 
 const app = express();
-const events=[];
+const events = [];
 
 app.use(bodyParser.json());
 
-
-
-app.use('/graphql',graphQlHttp({
-    schema:buildSchema(`
+app.use(
+  "/graphql",
+  graphQlHttp({
+    schema: buildSchema(`
     type Event {
         _id:ID!
         title:String!
@@ -24,6 +26,13 @@ app.use('/graphql',graphQlHttp({
 
     }
 
+    type User {
+        _id:ID!
+        email:String!
+        password:String
+    }
+
+
     input EventInput {
         title:String!
         description:String! 
@@ -32,14 +41,22 @@ app.use('/graphql',graphQlHttp({
 
     }
 
+    input UserInput {
+        email:String! 
+        password:String!
+    }
+
+  
     type RootQuery {
         events:[Event!]!
+        users:[User!]!
 
     }
 
     type RootMutation{
 
         createEvent(eventInput:EventInput):Event
+        createUser(userInput:UserInput):User
 
     }
      schema {
@@ -48,47 +65,90 @@ app.use('/graphql',graphQlHttp({
 
      }
     `),
-    rootValue:{
-        events:()=>{
-           return Event.find().then(events=>{
-               return events.map(event=>{
-                   return { ...event._doc }
-               })
-           }).catch(err=>{
-               throw err;
-           })
-        },
-        createEvent:(args)=>{
+    rootValue: {
+      events: () => {
+        return Event.find()
+          .then(events => {
+            return events.map(event => {
+              return { ...event._doc };
+            });
+          })
+          .catch(err => {
+            throw err;
+          });
+      },
+      createEvent: args => {
+        const event = new Event({
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: +args.eventInput.price,
+          date: new Date(args.eventInput.date),
+          creator: "5d248d3f543a9b0e7cb5248e"
+        });
 
-            const event=new Event({
+        let createdEvent;
 
-                title:args.eventInput.title,
-                description:args.eventInput.description,
-                price:+args.eventInput.price,
-                date:new Date(args.eventInput.date)
-            })
+        return event
+          .save()
+          .then(eventSaved => {
+            createdEvent = { ...eventSaved._doc };
+            return User.findById("5d248d3f543a9b0e7cb5248e");
+            // console.log(eventSaved);
+            // return { ...eventSaved._doc };
+          })
+          .then(user => {
+            if (!user) throw new Error("User not found");
+            console.log(user);
+            user.createdEvent.push(event);
+            return user.save();
+          })
+          .then(userSaved => {
+            return createdEvent;
+          })
+          .catch(err => {
+            console.log(err);
+            throw err;
+          });
+      },
 
-           return event.save().then(eventSaved=>{
-               console.log(eventSaved)
-               return {...eventSaved._doc}
-           }).catch(err=>{
-               console.log(err)
-               throw err
-            })
-         
-           
+      createUser: args => {
+        return User.findOne({ email: args.userInput.email }).then(user => {
+          if (user) throw new Error("User already exist");
+          else {
+            return bcrypt
+              .hash(args.userInput.password, 12)
+              .then(hashedPassword => {
+                const user = new User({
+                  email: args.userInput.email,
+                  password: hashedPassword
+                });
 
-        }
+                return user.save();
+              })
+              .then(userSaved => {
+                return { ...userSaved._doc, password: null };
+              })
+              .catch(err => {
+                throw err;
+              });
+          }
+        });
+      }
     },
 
-    graphiql:true,
-  
-    
-}))
+    graphiql: true
+  })
+);
 
-mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-3c0xg.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
-)
-.then(()=>console.log('connect to mongo'))
- .catch(err=>console.log(err))
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${
+      process.env.MONGO_PASSWORD
+    }@cluster0-3c0xg.mongodb.net/${
+      process.env.MONGO_DB
+    }?retryWrites=true&w=majority`
+  )
+  .then(() => console.log("connect to mongo"))
+  .catch(err => console.log(err));
 
 app.listen(3000, () => console.log("server is runnning"));
--- models/event.js
+++ models/event.js
@@ -1,29 +1,30 @@
-const mongoose=require('mongoose')
+const mongoose = require("mongoose");
 
-const Schema=mongoose.Schema
+const Schema = mongoose.Schema;
 
-const  EventSchema=new Schema({
+const EventSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
 
-    title:{
-        type:String,
-        required:true
-    },
+  description: {
+    type: String,
+    required: true
+  },
 
-    description:{
-        type:String,
-        required:true
-    },
+  price: {
+    type: Number,
+    required: true
+  },
+  date: {
+    type: Date,
+    required: true
+  },
+  creator: {
+    type: Schema.Types.ObjectId,
+    ref: "users"
+  }
+});
 
-    price:{
-        type:Number,
-        required:true
-    },
-    date:{
-        type:Date,
-        required:true
-    }
-
-})
-
-module.exports=mongoose.model('events',EventSchema)
-
+module.exports = mongoose.model("events", EventSchema);
