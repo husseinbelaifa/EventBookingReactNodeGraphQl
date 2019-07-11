38c9b2b0840d80f8ebf417e41d88840cd9cc1315
belaifa el hussein <belaifahocine@gmail.com>
2019-07-11 10:15:08 +0200

    add event & booking page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 import "./App.css";
-import Auth from "./pages/Auth";
+import authPage from "./pages/Auth";
+import eventPage from "./pages/Event";
+import bookingPage from "./pages/Booking";
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Redirect from="/" to="/auth" exact />
-        <Route path="/auth" component={Auth} />
-        <Route path="/events" component={null} />
-        <Route path="/bookings" component={null} />
+        <Route path="/auth" component={authPage} />
+        <Route path="/events" component={eventPage} />
+        <Route path="/bookings" component={bookingPage} />
       </Switch>
     </BrowserRouter>
   );
---
+++ frontend/src/pages/Event.js
@@ -0,0 +1,9 @@
+// @flow
+import React from "react";
+class Auth extends React.Component {
+  render() {
+    return <h1>The Event Page</h1>;
+  }
+}
+
+export default Auth;
